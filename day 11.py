381a6951e8d5836b9c341650ae618618e2f513cc
JaeMoon <ljm96040@gmail.com>
2025-02-24 14:47:03 +0900

    v0.4 day11 ml

--- day 11.py
+++ day 11.py
@@ -1,25 +1,25 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 
-def square(n) -> int:
-    return n*n
+ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
+#print(type(ls)) 출력결과 <class 'pandas.core.frame.DataFrame'>
+#print(ls)
+X= ls[["GDP per capita (USD)"]].values
+y = ls[["Life satisfaction"]].values
+#print(x)
 
-df = pd.DataFrame(
-    [[4, 7, 10],
-     [5, 88, 11],
-     [16, 9, 12]], index=[1, 2, 3], columns=['a', 'b', 'c']
-)
-print(df)
-#print(df.apply(square))
+ls.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23_500,62_500,4, 9])
+plt.show()
 
+model = LinearRegression()
+# model = KNeighborsRegressor(n_neighbors=3)
+model.fit(X, y)
 
-
-# df2 = pd.melt(df).rename(columns={'variable' : 'var' , 'value' : 'val'}).query('val>=10').sort_values('val',ascending=False)
-#
-# print(df2)
-
-
-df3 = df.iloc[1:2]
-#df3 = df.loc[1:2]
-#df3 = df.iloc[:, [0.2]]
-print(df3)
+X_new = [[31721.3]]  # ROK 2020
+print(model.predict(X_new))
+# LinearRegression 5.90
+# KNeighborsRegressor 5.70

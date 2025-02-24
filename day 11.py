6613e8f04bca066ae999c3aa238e374cf0236ed1
JaeMoon <ljm96040@gmail.com>
2025-02-24 14:12:23 +0900

    v0.31 day11 pandas

--- day 11.py
+++ day 11.py
@@ -1,9 +1,25 @@
 import numpy as np
 import pandas as pd
 
+def square(n) -> int:
+    return n*n
+
 df = pd.DataFrame(
     [[4, 7, 10],
-     [5, 8, 11],
-     [6, 9, 12]], index=[1, 2, 3], columns=['a', 'b', 'c']
+     [5, 88, 11],
+     [16, 9, 12]], index=[1, 2, 3], columns=['a', 'b', 'c']
 )
 print(df)
+#print(df.apply(square))
+
+
+
+# df2 = pd.melt(df).rename(columns={'variable' : 'var' , 'value' : 'val'}).query('val>=10').sort_values('val',ascending=False)
+#
+# print(df2)
+
+
+df3 = df.iloc[1:2]
+#df3 = df.loc[1:2]
+#df3 = df.iloc[:, [0.2]]
+print(df3)

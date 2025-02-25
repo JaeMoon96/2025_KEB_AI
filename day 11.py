8c9e1f9d2e1ccd7a912b7724d8983d4f73484cc2
JaeMoon <ljm96040@gmail.com>
2025-02-25 16:32:17 +0900

    v0.6 day11 ml

--- day 11.py
+++ day 11.py
@@ -1,24 +1,24 @@
+# Assignment
+# v0.7) v0.6의 최근접이웃모델과 같이 동작하는 커스텀 클래스를 설계하시오.
+
 # from sklearn.linear_model import LinearRegression
-import numpy as np
-# import jmlearn as jm
-from jmlearn import LinearRegression
+# import jmlearn as tg
+# from jmlearn import LinearRegression
+# from jmlearn import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsRegressor
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsRegressor
 
 ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
-#print(type(ls)) 출력결과 <class 'pandas.core.frame.DataFrame'>
-#print(ls)
 X = ls[["GDP per capita (USD)"]].values
 y = ls[["Life satisfaction"]].values
-#print(x)
 
 ls.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
-plt.axis([23_500,62_500,4, 9])
+plt.axis([23500, 62500, 4, 9])
 plt.show()
 
-model = LinearRegression()
-# model = KNeighborsRegressor(n_neighbors=3)
+#model = LinearRegression()
+model = KNeighborsRegressor()
 model.fit(X, y)
 
 X_new = [[31721.3]]  # ROK 2020
